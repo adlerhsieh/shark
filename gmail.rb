d4662a50a6300d1aa238e8b775c55425795c3b98
Adler <nkj20932@hotmail.com>
2018-02-13 22:10:07 +1100

    add gmail example

---
+++ gmail.rb
@@ -0,0 +1,72 @@
+require 'google/apis/gmail_v1'
+require 'googleauth'
+require 'googleauth/stores/file_token_store'
+require 'fileutils'
+require 'pry'
+
+class Service
+  def initialize
+    @service = Google::Apis::GmailV1::GmailService.new
+    @service.client_options.application_name = APPLICATION_NAME
+    @service.authorization = authorize
+  end
+
+  def user_id
+    'me'
+  end
+
+  OOB_URI = 'urn:ietf:wg:oauth:2.0:oob'
+  APPLICATION_NAME = 'Gmail API Ruby Quickstart'
+  CLIENT_SECRETS_PATH = '/Users/hsiehadler/projects/shark/google_api_client_secret.json'
+  CREDENTIALS_PATH = File.join(Dir.home, '.credentials', "gmail-ruby-quickstart.yaml")
+  SCOPE = Google::Apis::GmailV1::AUTH_GMAIL_READONLY
+  
+  ##
+  # Ensure valid credentials, either by restoring from the saved credentials
+  # files or intitiating an OAuth2 authorization. If authorization is required,
+  # the user's default browser will be launched to approve the request.
+  #
+  # @return [Google::Auth::UserRefreshCredentials] OAuth2 credentials
+  def authorize
+    FileUtils.mkdir_p(File.dirname(CREDENTIALS_PATH))
+
+    client_id = Google::Auth::ClientId.from_file(CLIENT_SECRETS_PATH)
+    token_store = Google::Auth::Stores::FileTokenStore.new(file: CREDENTIALS_PATH)
+    authorizer = Google::Auth::UserAuthorizer.new(
+      client_id, SCOPE, token_store)
+    user_id = 'default'
+    credentials = authorizer.get_credentials(user_id)
+    if credentials.nil?
+      url = authorizer.get_authorization_url(
+        base_url: OOB_URI)
+      puts "Open the following URL in the browser and enter the " +
+           "resulting code after authorization"
+      puts url
+      code = gets
+      credentials = authorizer.get_and_store_credentials_from_code(
+        user_id: user_id, code: code, base_url: OOB_URI)
+    end
+    credentials
+  end
+
+  def method_missing(m, *args, &block)
+    @service.send(m.to_sym, *args, &block)
+  end
+
+  def messages
+    @service.list_user_messages('me', max_results: 3, q: "shark-tips")
+  end
+
+  def message(id)
+    @service.get_user_message('me', id)
+  end
+end
+
+service = Service.new
+messages = service.messages.messages
+
+raw_signals = messages.map do |m|
+  message = service.message(m.id)
+  body = message.payload.parts.map {|b| b.body.data }.join("\n")
+  body.include?("Signal") ? body : nil
+end.compact
