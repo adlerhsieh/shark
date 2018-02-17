6f8f5c1f3d39159069603f1ac2cb628d22c499df
Adler <nkj20932@hotmail.com>
2018-02-17 19:52:17 +1100

    add deployment config

--- config/deploy.rb
+++ config/deploy.rb
@@ -43,3 +43,11 @@ set :keep_releases, 3
 
 # Uncomment the following to require manually verifying the host key before first deploy.
 # set :ssh_options, verify_host_key: :secure
+
+namespace :deploy do
+  task :config do
+    execute "cp #{fetch(:deploy_to)}/shared/.env #{fetch(:deploy_to)}/current"
+  end
+end
+
+after :deploy, "deploy:config"
