339f3e7b1ba6c68fbc07700907df747029e755bb
Adler <nkj20932@hotmail.com>
2018-02-18 07:32:09 +1100

    update deployment

--- config/deploy.rb
+++ config/deploy.rb
@@ -53,7 +53,8 @@ namespace :deploy do
 
   task :restart do
     on roles(:web) do
-      execute "kill -9 `cat #{fetch(:deploy_to)}/shared/pids/puma.pid`"
+      execute "/etc/init.d/puma stop"
+      sleep 1
       execute "/etc/init.d/puma start"
     end
   end
