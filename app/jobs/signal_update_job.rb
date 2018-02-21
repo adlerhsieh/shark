b637aa4c7379d0885ef082edc163a075d41d3762
Adler <nkj20932@hotmail.com>
2018-02-22 08:06:38 +1100

    remove file and job

--- app/jobs/signal_update_job.rb
+++
@@ -1,9 +0,0 @@
-class SignalUpdateJob < ApplicationJob
-  queue_as :default
- 
-  def perform(signal)
-    price_list = IG::PriceCollect.new(signal).collect
-    IG::SignalUpdate.new(price_list, signal).update!
-  end
-
-end
