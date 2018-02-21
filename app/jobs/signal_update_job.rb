bd609c64ceb86e8426bacba1a69887497a9ab237
Adler <nkj20932@hotmail.com>
2018-02-22 00:00:32 +1100

    move price_collect stuff to lib

--- app/jobs/signal_update_job.rb
+++ app/jobs/signal_update_job.rb
@@ -2,21 +2,8 @@ class SignalUpdateJob < ApplicationJob
   queue_as :default
  
   def perform(signal)
-    IG::PriceUpdate.new(price_list(signal), signal).update!
+    price_list = IG::PriceCollect.new(signal).collect
+    IG::SignalUpdate.new(price_list, signal).update!
   end
-
-  private
-
-    def service
-      @service ||= IG::Service.new
-    end
-
-    def price_list(signal)
-      service.price(
-        signal.pair.ig_epic,
-        signal.created_at.strftime("%Y-%m-%d"),
-        (signal.created_at + 1.day).strftime("%Y-%m-%d")
-      )
-    end
 
 end
