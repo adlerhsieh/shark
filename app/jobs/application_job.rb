5be68a46070484c2708b87d50c80ab4037b6c4e8
Adler <nkj20932@hotmail.com>
2018-03-08 08:11:41 +1100

    include sidekiq worker

--- app/jobs/application_job.rb
+++ app/jobs/application_job.rb
@@ -1,5 +1,8 @@
 class ApplicationJob < ActiveJob::Base
 
+  # Try to solve "undefined method jid" for jobs
+  include Sidekiq::Worker
+
   def log
     @log ||= AuditLog.create(event: self.class)
   end
