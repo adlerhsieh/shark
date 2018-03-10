66f63733ba120b7ae168371e0150230288936321
Adler <nkj20932@hotmail.com>
2018-03-11 01:06:42 +1100

    add CRUD to positions

--- app/controllers/admin/positions_controller.rb
+++ app/controllers/admin/positions_controller.rb
@@ -1,7 +1,34 @@
 class Admin::PositionsController < Admin::BaseController
+  before_action :load_position, only: %i[edit update destroy]
 
   def index
+    @positions = Position.all
+  end
+
+  def new
     
   end
+
+  def create
+    
+  end
+
+  def edit
+    
+  end
+
+  def update
+    
+  end
+
+  def destroy
+    
+  end
+
+  private
+
+    def load_position
+      @position = Position.find(params[:id])
+    end
 
 end
