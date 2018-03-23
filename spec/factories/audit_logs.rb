6acc9c8e1a44ac0f642cb01e3093fcd6a5bc2ef6
Adler <nkj20932@hotmail.com>
2018-03-23 21:19:24 +1100

    add spec to audit_log

--- spec/factories/audit_logs.rb
+++ spec/factories/audit_logs.rb
@@ -1,7 +1,5 @@
 FactoryBot.define do
   factory :audit_log do
-    source_type 1
-    source_id 1
-    content "MyText"
+
   end
 end
--- spec/models/audit_log_spec.rb
+++ spec/models/audit_log_spec.rb
@@ -1,2 +1,30 @@
 RSpec.describe AuditLog, type: :model do
+
+  describe "#write" do
+    let!(:log) { create(:audit_log) }
+    
+    subject { log.write("foo") }
+
+    it "writes to the log" do
+      expect { subject }.to change { log.reload.content }.to("foo\n")
+    end
+  end
+
+  describe "#error" do
+    let!(:log) { create(:audit_log) }
+
+    subject do 
+      ex = StandardError.new("bar")
+      ex.set_backtrace("x")
+
+      log.error(ex)
+    end
+    
+    it "writes to the log" do
+      subject
+
+      expect(log.content).to include("bar")
+    end
+  end
+
 end
