75f4d6918378ef1f4d853f1c7f65339e3c2340dd
Adler <nkj20932@hotmail.com>
2018-03-28 20:24:03 +1100

    add sentry config

--- config/initializers/sentry_raven.rb
+++ config/initializers/sentry_raven.rb
@@ -1,5 +1,12 @@
 Raven.configure do |config|
 
   config.dsn = ENV["SENTRY_DSN"]
+  config.environments = %w[production]
+
+  config.silence_ready = true
+
+  config.excluded_exceptions += [
+    "ActionController::UnknownFormat"
+  ]
 
 end
